92a9182284616ea5b6895dba8fc400c91084ad4e
Sandra Hooper <sandrahooper@Sandras-MacBook-Pro.local>
2018-08-17 23:08:07 -0400

    update

---
+++ server/src/policies/AuthenticationControllerPolicy.js
@@ -0,0 +1,43 @@
+const Joi = require('joi');
+
+
+module.exports = {
+    register (req, res, next){
+        const schema = {
+            email:  Joi.string().email(),
+            password:   Joi.string().regex(
+                new RegExp('^[a-zA-Z0-9]{8,32}$')
+            )
+        }
+
+        const {error, value} = Joi.validate(req.body, schema)
+
+        if (error) {
+            switch(error.details[0].context.key){
+                case 'email':
+                    res.status(400).send({
+                        error: 'You must provide a valid email address'
+                    });
+                    break
+                case 'password':
+                    res.status(400).send({
+                        error: `The password provided failed to match the following rules:
+                            <br>
+                            1. It must containe the following characters: upper case, lower case
+                            <br>
+                            2. It must be between 8 and 32 characters in length
+                        `
+                });
+                    break
+                default:
+                    res.status(400).send({
+                        error: 'Invalid registration information'
+                    });
+            }
+        }else{
+            next()
+        }
+
+        
+    }
+}
